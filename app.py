1c52d136041054c72b209967c793b1ea74d59839
Hermartrey <120154213+Hermartrey@users.noreply.github.com>
2023-10-20 13:30:02 +0000

    add requirements.txt

--- app.py
+++ app.py
@@ -1,33 +1,8 @@
 from flask import Flask, render_template, jsonify
+from dummy import JOBS
 
 app = Flask(__name__)
 
-JOBS = [
-  {
-    "id": 1,
-    "title": "Python Developer",
-    "location": "philippines",
-    "salary": "100k",
-  },
-  {
-    "id": 2,
-    "title": "frontend engineer",
-    "location": "singapore",
-    "salary": "150k",
-  },
-  {
-    "id": 3,
-    "title": "backend engineer",
-    "location": "japan",
-    "salary": "180k",
-  },
-  {
-    "id": 4,
-    "title": "full stack developer",
-    "location": "united states",
-    "salary": "220k",
-  }
-]
 company_name = "Hr"
 
 @app.route("/")
---
+++ dummy.py
@@ -0,0 +1,26 @@
+JOBS = [
+  {
+    "id": 1,
+    "title": "Python Developer",
+    "location": "philippines",
+    "salary": "100k",
+  },
+  {
+    "id": 2,
+    "title": "frontend engineer",
+    "location": "singapore",
+    "salary": "150k",
+  },
+  {
+    "id": 3,
+    "title": "backend engineer",
+    "location": "japan",
+    "salary": "180k",
+  },
+  {
+    "id": 4,
+    "title": "full stack developer",
+    "location": "united states",
+    "salary": "220k",
+  }
+]
